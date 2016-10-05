9be2c72946da0edefccffe8b8d18426ee7aee305
marthamareal <marthamareal@gmail.com>
2016-10-05 14:04:27 -0700

    first commit

---
+++ app/src/main/java/mareal/mweso/Splashscreen.java
@@ -0,0 +1,35 @@
+package mareal.mweso;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Splashscreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        Thread timerThread = new Thread(){
+            public void run(){
+            try{
+   sleep(3000);
+}
+            catch(InterruptedException e){
+                e.printStackTrace();
+ }
+            finally{
+ Intent intent = new Intent(Splashscreen.this,MwesoGame.class);
+    startActivity(intent);
+     }
+    }
+  };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
