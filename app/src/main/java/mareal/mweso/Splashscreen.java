d8265bfb1e507a1688ded4edf874671c9bc1c80e
Davies Wabuluka <webdavies@gmail.com>
2016-10-06 04:07:17 -0700

    Update Splashscreen.java
    i have tried to comment where necessary to try and make it easy for the readers or future developers easy

--- app/src/main/java/mareal/mweso/Splashscreen.java
+++ app/src/main/java/mareal/mweso/Splashscreen.java
@@ -3,7 +3,7 @@ package mareal.mweso;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-
+//this handles the welcome screen that splashes the first time the app is started
 public class Splashscreen extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
