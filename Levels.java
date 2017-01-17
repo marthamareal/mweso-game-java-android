d8c9d5851a43eb80081110c879e2e14ac2d93579
mwesoteam <tv00024@gmail.com>
2017-01-17 14:00:49 -0800

    Add files via upload
    Java class for the levels

---
+++ Levels.java
@@ -0,0 +1,17 @@
+package com.example.fred.mwesogame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Frether on 1/2/2017.
+ */
+public class Levels extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.levels);
+
+    }
+    }
+
