4dd46b3528f40c30aa37273c35920ca726658fbe
MUGABI SAMUEL <samuelmugabi2@gmail.com>
2017-01-18 03:16:55 -0800

    Add files via upload

---
+++ AI.java
@@ -0,0 +1,22 @@
+package artint;
+
+public class AI {
+	 
+	public static int sum(int [] Array){
+		int sum = IntStream.of(a).sum();
+		System.out.println("The sum is " + sum);
+		}
+		return total;
+	}
+	public int difference(int  [] array, int total){
+		
+		int diff =12-total;
+		return diff;
+	}
+	//public int better(int [] array, int diff){}
+	public static void main(String [] args){
+		int [] magic={1,2,3};
+		int one =sum(magic);
+		System.out.println(one);
+	}
+}
