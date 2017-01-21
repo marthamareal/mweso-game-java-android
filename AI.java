09395ca317cedaf031eb947d50386280b5ab15fa
MUGABI SAMUEL <samuelmugabi2@gmail.com>
2017-01-20 21:08:47 -0800

    Add files via upload

--- AI.java
+++ AI.java
@@ -3,20 +3,37 @@ package artint;
 public class AI {
 	 
 	public static int sum(int [] Array){
-		int sum = IntStream.of(a).sum();
-		System.out.println("The sum is " + sum);
+		int total=0;
+		for(int i =0;i<3;i++){
+			total=total+ Array[i];
 		}
 		return total;
 	}
-	public int difference(int  [] array, int total){
-		
-		int diff =12-total;
+	public static int difference(int total){
+		int diff;
+		if (total<12){
+		diff =12-total;}
+		else{diff=total-12;}
 		return diff;
 	}
-	//public int better(int [] array, int diff){}
+	public int [] better(int [] array){
+		int best=0;
+		int [] K={0,0,0};
+		for (int i=0;i<10;i++){
+			//call AI method to play move which returns array of positions of player tokens
+			K=array;
+			int one =sum(array);
+			one = difference(one);
+		if (one < best){best=one;}
+		//i want if new array is smaller return that array else return old array
+		 }
+		return K;
+		
+	}
 	public static void main(String [] args){
-		int [] magic={1,2,3};
-		int one =sum(magic);
-		System.out.println(one);
+		//int [] magic={6,2,3};
+		// i want to call sum then get difference 
+				//magnitude
+		//System.out.println(best);
 	}
 }
